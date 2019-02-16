d09f6602f0075ba8edf2c595ad64648778f88934
Trent Moss <trentmoss05@gmail.com>
2019-02-16 03:55:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student = Student.new
+    student = self.new
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
